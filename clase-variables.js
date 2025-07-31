06efccc5166e897486115fffa42de8c1fd36985a
Claudia <claudiasandoval150998@gmail.com>
2025-07-31 15:18:49 -0500

    first commit

---
+++ clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = "Woody";
+console.log(cajaDeAndy);
+
+
+//Lo NO permitido
+let c= "Woody";
+let cda = "Woody";
+
+
+//Lo permitido
+
+let primerTrasteoDeAndy = "Woody";
+let urlDelUsuario = "https://platzi.com/cursos/javascript/anatomia-de-una-variable/";
+let idDelUsuario = "andy123";
---
+++ strings.js
@@ -0,0 +1,15 @@
+// Tipo de dato: String
+
+let string1 = "Hola, Platzi";
+let string2 = '¡Bienvenido a la clase de variables!';
+let string3 = "string1 + string2";
+
+
+//console.log(string1);
+//console.log(string2);
+//console.log(string3);
+
+let frase = "JavaScript es Extremadamente divertido";
+console.log(frase.length);
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
