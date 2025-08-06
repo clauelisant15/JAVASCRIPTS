02fdbc12437d1557bc37e1afc20ab41dd491cd9c
Claudia Elizabeth <i202507477@cibertec.edu.pe>
2025-08-05 22:03:22 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+let nombre ="Claudia";
+
+if (nombre === "Claudia") {
+    console.log("Hola Claudia, bienvenida de nuevo");
+}else if (nombre === "Claudia") {
+    console.log("Hola Claudia");
+} else {
+    console.log("Nomre no encontrado");
+}
